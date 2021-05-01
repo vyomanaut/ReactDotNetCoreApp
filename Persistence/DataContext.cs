b0bda3e4578e0e81c16a7ec8710bac6d1bf14315
Samit Shirsat <samitshirsatfb@gmail.com>
2021-05-01 12:49:27 +0530

    1st Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
